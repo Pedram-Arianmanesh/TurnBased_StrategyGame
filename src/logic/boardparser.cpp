b295e6c4469d347cca8fa9692c170df6351e98bd
Pedram Arianmanesh <PedramArianmanesh@gmail.com>
2025-07-22 18:48:42 +0330

    correct boardparser logic

--- src/logic/boardparser.cpp
+++ src/logic/boardparser.cpp
@@ -4,24 +4,6 @@
 #include <QTextStream>
 #include <QDebug>
 
-struct CellPattern {
-    int row;
-    int col;
-    int line;
-    int index;
-};
-
-static const std::vector<CellPattern> cellPatterns = {
-    {0, 0, 1,  1}, {0, 1, 1,  6}, {0, 2, 1, 11}, {0, 3, 1, 16}, {0, 4, 1, 21},
-    {1, 0, 2,  4}, {1, 1, 2,  9}, {1, 2, 2, 14}, {1, 3, 2, 19},
-    {2, 0, 3,  1}, {2, 1, 3,  6}, {2, 2, 3, 11}, {2, 3, 3, 16}, {2, 4, 3, 21},
-    {3, 0, 4,  4}, {3, 1, 4,  9}, {3, 2, 4, 14}, {3, 3, 4, 19},
-    {4, 0, 5,  1}, {4, 1, 5,  6}, {4, 2, 5, 11}, {4, 3, 5, 16}, {4, 4, 5, 21},
-    {5, 0, 6,  4}, {5, 1, 6,  9}, {5, 2, 6, 14}, {5, 3, 6, 19},
-    {6, 0, 7,  1}, {6, 1, 7,  6}, {6, 2, 7, 11}, {6, 3, 7, 16}, {6, 4, 7, 21},
-    {7, 0, 8,  4}, {7, 1, 8,  9}, {7, 2, 8, 14}, {7, 3, 8, 19},
-    {8, 0, 9,  1}, {8, 1, 9,  6}, {8, 2, 9, 11}, {8, 3, 9, 16}, {8, 4, 9, 21}
-};
 Board BoardParser::parseBoard(const QString &filePath) {
     QFile file(filePath);
     Board board(9);
@@ -42,33 +24,29 @@ Board BoardParser::parseBoard(const QString &filePath) {
             lines.append(line);
     }
     file.close();
-
-    for (const auto &pattern : cellPatterns) {
-        if (pattern.line >= lines.size()) continue;
-        const QString &line = lines[pattern.line];
-
-        TerrainType terrain = TerrainType::Free;
-        int owner = 0;
-
-        static const std::vector<int> offsets = {-1, 0, 1};
-        for (int di : offsets) {
-            int l = pattern.line + di;
-            if (l < 0 || l >= lines.size()) continue;
-
-            for (int dj : offsets) {
-                int c = pattern.index + dj;
-                if (c < 0 || c >= lines[l].size()) continue;
-
-                QChar ch = lines[l][c];
+    for (int i = 1; i < 10; i++){
+        int slash_num = 0;
+        for (int j = 0; j < lines[i].length(); ++j) {
+            if (lines[i][j] == "/"){
+                if (j + 1 >= lines[i].length()) {
+                    qWarning() << "BoardParser: '/' found at end of line " << i << ", skipping (cannot read next character).";
+                    break;
+                }
+                TerrainType terrain = TerrainType::Free;
+                int owner = 0;
+                QChar ch = lines[i][j + 1];
                 if (ch == '#') terrain = TerrainType::Rock;
                 else if (ch == '~') terrain = TerrainType::Water;
                 else if (ch == '1') owner = 1;
                 else if (ch == '2') owner = 2;
+                board[i - 1].emplace_back(i - 1, slash_num, terrain, owner);
+
+                slash_num++;
+
             }
         }
-
-        board[pattern.row].emplace_back(pattern.row, pattern.col, terrain, owner);
     }
+
 
     return board;
 }
